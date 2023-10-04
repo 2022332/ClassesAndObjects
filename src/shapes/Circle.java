f211d10f7ecb9a423cb2723ce60ada04cc79e4a5
2022332 <2022332@student.cct.ie>
2023-10-04 19:18:29 +0100

    Creation of the subclass Circle and its attributes

---
+++ src/shapes/Circle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapes;
+
+/**
+ *
+ * @author Ari
+ */
+public class Circle {
+    private int radius;
+
+    public int getRadius() {
+        return radius;
+    }
+    
+    public double getArea(){
+        return (3.141579 * radius * radius);
+    }
+}
