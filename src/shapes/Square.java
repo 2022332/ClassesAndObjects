5a740648b34d42d9a042b4e224316ed9d0e523e2
2022332 <2022332@student.cct.ie>
2023-10-13 12:03:53 +0100

    Creation of a interface Shapes 2D and implementing it on the shapes.

---
+++ src/shapes/Square.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shapes;
+
+/**
+ *
+ * @author Ari
+ */
+public class Square extends Rectangle{
+    
+    public Square(boolean isFilled, String colour, int length) {
+        super(isFilled, colour, length, length);
+    }
+}
